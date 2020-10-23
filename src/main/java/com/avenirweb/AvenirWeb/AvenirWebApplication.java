a171633229bc8097b1cc402e7ffa2ee3337df4ae
HolyLoki <forwoermind@gmail.com>
2020-10-23 17:11:11 +0600

    Initial commit

---
+++ src/main/java/com/avenirweb/AvenirWeb/AvenirWebApplication.java
@@ -0,0 +1,13 @@
+package com.avenirweb.AvenirWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvenirWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvenirWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avenirweb/AvenirWeb/AvenirWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avenirweb.AvenirWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvenirWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
